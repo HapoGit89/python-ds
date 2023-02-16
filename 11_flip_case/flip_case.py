d3a9f8f94401623911023bb50e7133cdff8e437d
Hannes <hannes.porombka@gmail.com>
2023-02-16 13:30:44 +0100

    finished up until exercise 13

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -1,4 +1,11 @@
 def flip_case(phrase, to_swap):
+    phrase_list = list(phrase)
+    flipped_list = [x.lower() if x == to_swap and x.isupper() == True else x.upper() if x == to_swap else x for x in phrase_list]
+    return ''.join(flipped_list)
+
+
+
+
     """Flip [to_swap] case each time it appears in phrase.
 
         >>> flip_case('Aaaahhh', 'a')
--- 13_capitalize/capitalize.py
+++ 13_capitalize/capitalize.py
@@ -1,4 +1,9 @@
 def capitalize(phrase):
+    phrase_list = list(phrase)
+    phrase_list[0] = phrase_list[0].upper()
+    return ''.join(phrase_list)
+
+
     """Capitalize first letter of first word of phrase.
 
         >>> capitalize('python')
