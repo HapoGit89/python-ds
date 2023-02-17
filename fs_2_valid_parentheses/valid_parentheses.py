805536e4100d4af7b9acfa756bbefa9a46919a1f
Hannes <hannes.porombka@gmail.com>
2023-02-17 15:34:27 +0100

    fs2 completed

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -1,4 +1,11 @@
 def valid_parentheses(parens):
+    if (parens.count("(") + parens.count(")")) %2  != 0:
+        return False
+    elif parens[0] == ")":
+        return False
+    else:
+        return True
+    
     """Are the parentheses validly balanced?
 
         >>> valid_parentheses("()")
